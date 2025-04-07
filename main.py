d3eab1c11f188aa339f1d3ce537e423c5d3d5bd9
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-04-07 11:58:01 +0200

    Added clock.tick

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     initialitation = pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

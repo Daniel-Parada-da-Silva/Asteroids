e84133f878b8673f5065c3a3518f60e8774983be
Daniel-Parada-da-Silva <daniel.parada.da.silva@gmail.com>
2025-04-09 22:17:44 +0200

    Cooldown added

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cd = 0
     
     # in the player class
     def triangle(self):
@@ -31,20 +32,23 @@ class Player(CircleShape):
         self.position += right * PLAYER_SPEED * 0.75 * dt
     
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        velocity = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity = velocity * PLAYER_SHOOT_SPEED
+        if self.cd <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity = velocity * PLAYER_SHOOT_SPEED
+            self.cd = PLAYER_SHOOT_COOLDOWN
     
     def update(self, dt):
+        self.cd -= dt
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(-dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
         if keys[pygame.K_q]:
             self.lateral_move(dt)
